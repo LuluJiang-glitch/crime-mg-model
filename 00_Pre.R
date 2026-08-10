f0c42a4653fda5367d1b58af5e0ee5550d3b3743
LuluJiang-glitch <lulu.jiang001@gmail.com>
2026-08-10 11:02:25 +0300

    Update 00_Pre.R

--- 00_Pre.R
+++ 00_Pre.R
@@ -1,8 +1,18 @@
-# Crime Data
+file_arg <- grep("^--file=", commandArgs(trailingOnly = FALSE), value = TRUE)
+if (length(file_arg) > 0) {
+  base_dir <- dirname(normalizePath(sub("^--file=", "", file_arg[1])))
+} else {
+  base_dir <- normalizePath(getwd())
+}
+
+setwd(base_dir)
+prepare_dir <- file.path(base_dir, "RData", "Prepare")
+source(file.path(base_dir, "metric_graph.R"))
+
+# Crime Data 01_data_city.RData
 # library(tidyverse)
-# base_path <- "~/Desktop/Crime/Data/Police_Aichive_all"
+# base_path <- "" # The path you download the crime data
 # folders <- list.dirs(base_path, recursive = FALSE)
-# 
 # read_month <- function(folder) {
 #   files <- list.files(folder, pattern = "street.*\\.csv$", full.names = TRUE)
 #   
@@ -11,14 +21,22 @@
 #     drop_na() %>%
 #     count(Month, Longitude, Latitude, `Crime type`, name = "count")
 # }
-# 
 # monthly_data <- map_dfr(folders, read_month)
 # yearly_data <- monthly_data %>%
 #   mutate(Year = substr(Month, 1, 4)) %>%
 #   group_by(Year, Longitude, Latitude, `Crime type`) %>%
 #   summarise(count = sum(count), .groups = "drop")
-# save(yearly_data, file = "~/Desktop/Crime/code_final/RData/Prepare/01_crime_yearly_data_all.RData")
-load("~/Desktop/Crime/code_final/RData/Prepare/01_crime_yearly_data_all.RData")
+# data_city <- yearly_data %>%
+#   filter(Year %in% as.character(model_years)) %>%
+#   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+#   filter(rowSums(st_within(geometry, boundary, sparse = FALSE)) > 0) %>%
+#   mutate(
+#     Longitude = st_coordinates(.)[, 1],
+#     Latitude = st_coordinates(.)[, 2],
+#     Year = as.numeric(Year)
+#   )
+# save(data_city, file = file.path(prepare_dir, "01_data_city.RData"))
+
 
 
 
@@ -30,41 +48,79 @@ library(osmdata)
 library(dplyr)
 library(MetricGraph)
 
-source("~/Desktop/Crime_UK/Code/ExampleCode/LinesMetricGraph-main/metric_graph.R")
 uk <- ne_states(country = "united kingdom", returnclass = "sf")
 london <- uk[uk$region == "Greater London", ]
 aa <- london[london$name %in% "City", ]
 boundary <- st_transform(aa, crs = 4326) %>% st_union()
 boundary_sf <- st_as_sf(boundary)
 
-path <- "~/Desktop/Crime/code_final/RData/Prepare/02_graph_City.RData"
+path <- file.path(prepare_dir, "02_graph_City_L100.RData")
 dir.create(dirname(path), recursive = TRUE, showWarnings = FALSE)
 
+graph_buffer_m <- 100
+
+boundary_graph_sf <- boundary_sf %>%
+  st_transform(27700) %>%
+  st_buffer(dist = graph_buffer_m) %>%
+  st_make_valid()
+
+boundary_graph_osm <- boundary_graph_sf %>% st_transform(4326)
+
+ggplot() +
+  geom_sf(
+    data = boundary_graph_osm,
+    fill = NA,
+    color = "red",
+    linewidth = 1
+  ) +
+  geom_sf(
+    data = boundary_sf,
+    fill = NA,
+    color = "blue",
+    linewidth = 1
+  ) +
+  theme_minimal() +
+  labs(
+    title = "Original and Buffered Boundaries",
+    subtitle = paste0("Buffer = ", graph_buffer_m, " m"),
+    color = NULL
+  )
+
+
 if (!file.exists(path)) {
-  call <- st_bbox(boundary) %>%
+  call <- boundary_graph_osm %>%
+    st_bbox() %>%
     opq() %>%
     add_osm_feature(key = "highway")
   
-  lines_inbound <- osmdata_sp(call)$osm_lines %>%
+  lines_inbound1 <- osmdata_sf(call)$osm_lines
+  
+  lines_inbound2 <- lines_inbound1 %>%
     st_as_sf() %>%
+    st_transform(4326)
+  
+  lines_inbound <- lines_inbound2 %>%
     dplyr::select(geometry) %>%
-    st_intersection(boundary_sf) %>%
+    st_intersection(boundary_graph_osm) %>%
     as_Spatial()
   
-  graph_pre <- graph_components$new(edges = lines_inbound, perform_merges = TRUE)
+  graph_pre <- graph_components$new(
+    edges = lines_inbound,
+    perform_merges = TRUE
+  )
+  
   graph <- graph_pre$get_largest()
+  graph$plot(type = "mapview", vertex_size = 0) + mapview::mapview(
+    boundary_sf,
+    color = "red",
+    col.regions = NA,
+    lwd = 3,
+    layer.name = "Original boundary"
+  )
   
   summary(graph)
-  save(graph, boundary, boundary_sf, file = path)
+  save(graph, file = path)
+  
 } else {
   load(path)
 }
-
-# graph$plot(vertex_size = 0)
-
-
-
-
-
-
-
